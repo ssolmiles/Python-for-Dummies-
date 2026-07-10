b96ac4b135b3df54718a9a0200babed770b66689
Geres Maria Gutierrez <geresmariapgutierrez@iskolarngbayan.pup.edu.ph>
2026-07-11 03:43:01 +0800

    Dummy #1

---
+++ From-Dummy-Solutions.py
@@ -0,0 +1,11 @@
+#Beginner Approach
+
+def dummy_function_power(base, exponent = 2):
+    return base ** exponent
+
+print(dummy_function_power(3))  
+print(dummy_function_power(3, 3))  
+
+#Can be manually replace the exponent
+#ex.
+print(dummy_function_power(3, exponent=4))
