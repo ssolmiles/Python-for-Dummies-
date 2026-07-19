86b67289ebd452c53de4da10223893e767e2be9e
Geres Maria Gutierrez <geresmariapgutierrez@iskolarngbayan.pup.edu.ph>
2026-07-19 22:07:40 +0800

    Dummy Puzzles d-2

--- Dummy-Python-Puzzle.py
+++ Dummy-Python-Puzzle.py
@@ -65,3 +65,40 @@ print(filter_even_length_strings(["cat", "dog", "fish", "elephant"]))
 def reverse_elements(input_nums: list[int]) -> list[int]:
     return input_nums[::-1]
 print(reverse_elements([1, 2, 3, 4, 5]))
+
+
+# puzzle 8 
+
+def filter_type_str(input_list: list[str | int]) -> list[str]:
+    result = []
+    for item in input_list:
+        if isinstance(item, str):
+            result.append(item)
+    return result
+
+print(filter_type_str(["hello", 1, 2, "www"]))
+
+
+# puzzle 9
+
+def string_to_morse_code(input_str: str) -> str:
+    morse_dict = { "a": ".-", "b": "-...", "c": "-.-.", "d": "-..", "e": ".", "f": "..-.", "g": "--.", "h": "....",
+                   "i": "..", "j": ".---", "k": "-.-", "l": ".-..", "m": "--", "n": "-.", "o": "---", "p": ".--.",
+                   "q": "--.-", "r": ".-.", "s": "...", "t": "-", "u": "..-", "v": "...-", "w": ".--", "x": "-..-",
+                   "y": "-.--", "z": "--..", "0": "-----", "1": ".----", "2": "..---", "3": "...--", "4": "....-",
+                   "5": ".....", "6": "-....", "7": "--...", "8": "---..", "9": "----.", ".": ".-.-.-", ",": "--..--",
+                   "?": "..--..", "'": ".----.", "!": "-.-.--", "/": "-..-.", "(": "-.--.", ")": "-.--.-", "&": ".-...",
+                   ":": "---...", ";": "-.-.-.", "=": "-...-", "+": ".-.-.", "-": "-....-", "_": "..--.-", "\"": ".-..-.",
+                   "$": "...-..-", "@": ".--.-." }
+
+    words = input_str.split(" ")
+    morse_per_word = [
+        " ".join(morse_dict.get(ch.lower(), "") for ch in word if ch.lower() in morse_dict)
+        for word in words
+    ]
+    morse_sentence = " / ".join(morse_per_word)
+    return morse_sentence
+
+
+print(string_to_morse_code("HELLO, WORLD!"))
+
